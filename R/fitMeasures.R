00bc8170fb71ca01ecc401f31367c02eea1e19dd
Kucharssim <kucharssim@gmail.com>
2023-09-13 18:06:28 +0200

    update fit measures

--- R/fitMeasures.R
+++ R/fitMeasures.R
@@ -1,64 +1,23 @@
-#' @name fit
-#' @rdname fit
+#' @name fitMeasures
+#' @rdname fitMeasures
 #'
-#' @title Fit measures of distributions
+#' @title fitMeasures measures of distributions
 #'
 #' @description
-#' Measures of absolute and relative fit of a distribution to the data
+#' Measures of absolute and relative fitMeasures of a distribution to the data
 #'
 #' @param distribution Object of class `jaspDistribution`.
 #' @param data Data.
-#' @param estimated Logical (default `FALSE`): are parameters of the distribution estimated using the same data set as the one used for computing the fit measure?
+#' @param estimated Logical (default `FALSE`): are parameters of the distribution estimated using the same data set as the one used for computing the fitMeasures measure?
 #'
 NULL
 
-#' @rdname fit
-#' @export
-AIC.jaspDistribution <- function(distribution, data) {
-  data <- na.omit(data)
-  lik <- likelihood(distribution, data, log = TRUE, scaling = -2)
-  npar <- free(distribution$parameters) |> length()
-
-  return(2 * npar - lik)
-}
-
-#' @rdname fit
-#' @export
-BIC.jaspDistribution <- function(distribution, data) {
-  data <- na.omit(data)
-  lik <- likelihood(distribution, data, log = TRUE, scaling = -2)
-  npar <- free(distribution$parameters) |> length()
-
-  return(log(length(data)) * npar - lik)
-}
-
-#' @rdname fit
-#' @export
-andersonDarling <- function(distribution, data, estimated = FALSE) {
-  result <- goftest::ad.test(data, null = cdf, distribution = distribution, estimated = estimated)
-  return(data.frame(test="ad", statistic=unname(result$statistic), pvalue=result$p.value))
-}
-
-#' @rdname fit
-#' @export
-cramerVonMises <- function(distribution, data, estimated = FALSE) {
-  result <- goftest::cvm.test(data, null = cdf, distribution = distribution, estimated = estimated)
-  return(data.frame(test="cvm", statistic=unname(result$statistic), pvalue=result$p.value))
-}
-
-#' @rdname fit
-#' @export
-kolmogorovSmirnov <- function(distribution, data) {
-  result <- stats::ks.test(x = data, y = cdf, distribution = distribution, alternative = "two.sided")
-  return(data.frame(test="ks", statistic=unname(result$statistic), pvalue=result$p.value))
-}
-
-#' @rdname fit
 #' @export
 fitMeasures <- function(distribution, data, estimated = FALSE) {
   UseMethod("fitMeasures")
 }
 
+#' @rdname fitMeasures
 #' @export
 fitMeasures.jaspDistribution <- function(distribution, data, estimated = FALSE) {
   ad  <- andersonDarling  (distribution, data, estimated)
@@ -70,3 +29,45 @@ fitMeasures.jaspDistribution <- function(distribution, data, estimated = FALSE) 
   )
   return(result)
 }
+
+#' @rdname fitMeasures
+#' @export
+AIC.jaspDistribution <- function(distribution, data) {
+  data <- na.omit(data)
+  lik <- likelihood(distribution, data, log = TRUE, scaling = -2)
+  npar <- free(distribution$parameters) |> length()
+
+  return(2 * npar - lik)
+}
+
+#' @rdname fitMeasures
+#' @export
+BIC.jaspDistribution <- function(distribution, data) {
+  data <- na.omit(data)
+  lik <- likelihood(distribution, data, log = TRUE, scaling = -2)
+  npar <- free(distribution$parameters) |> length()
+
+  return(log(length(data)) * npar - lik)
+}
+
+#' @rdname fitMeasures
+#' @export
+andersonDarling <- function(distribution, data, estimated = FALSE) {
+  result <- goftest::ad.test(data, null = cdf, distribution = distribution, estimated = estimated)
+  return(data.frame(test="ad", statistic=unname(result$statistic), pvalue=result$p.value))
+}
+
+#' @rdname fitMeasures
+#' @export
+cramerVonMises <- function(distribution, data, estimated = FALSE) {
+  result <- goftest::cvm.test(data, null = cdf, distribution = distribution, estimated = estimated)
+  return(data.frame(test="cvm", statistic=unname(result$statistic), pvalue=result$p.value))
+}
+
+#' @rdname fitMeasures
+#' @export
+kolmogorovSmirnov <- function(distribution, data) {
+  result <- stats::ks.test(x = data, y = cdf, distribution = distribution, alternative = "two.sided")
+  return(data.frame(test="ks", statistic=unname(result$statistic), pvalue=result$p.value))
+}
+
